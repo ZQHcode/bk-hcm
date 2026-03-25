9186361ca935986a3159953028e9dcd05291cd1d
ZQHcode <1550050864@qq.com>
2026-03-25 16:59:23 +0800

    feat: 新增权限模版dataservice接口和sql --story=132944757

--- cmd/data-service/service/audit/cloud/create_resource_update_audit.go
+++ cmd/data-service/service/audit/cloud/create_resource_update_audit.go
@@ -101,6 +101,8 @@ func (ad Audit) buildUpdateAuditInfo(kt *kit.Kit, resType enumor.AuditResourceTy
 		audits, err = ad.permissionPolicyLibraryUpdateAuditBuild(kt, updates)
 	case enumor.AccountSecretAuditResType:
 		audits, err = ad.accountSecretUpdateAuditBuild(kt, updates)
+	case enumor.PermissionTemplateAuditResType:
+		audits, err = ad.permissionTemplateUpdateAuditBuild(kt, updates)
 
 	default:
 		return nil, fmt.Errorf("cloud resource type: %s not support", resType)
